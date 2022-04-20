c52a49b333683b7abcfd71be53f3ec6f20c95c85
Mohamed Reda <alprofmohamed123@gmail.com>
2022-04-20 12:01:24 +0200

    basic routes and http methods

--- index.js
+++ index.js
@@ -1,13 +1,39 @@
 import express, {json} from 'express'
 const app = express()
 const PORT = 3000
+// get json file and preview
+import data from './data/mock.json'
 
 
+// Get Post Put Delete Methods
+// first GET method
+app.get('/', (req,res)=>{
+    res.send('this is a GET request from the path /')
+})
+// first POST method
+app.post('/create', (req,res)=>{
+    res.send('this is a POST request from the path /create')
+})
+// first PUT method
+app.put('/edit', (req,res)=>{
+    res.send('this is a PUT request from the path /edit')
+})
+// first DELETE method
+app.delete('/remove', (req,res)=>{
+    res.send('this is a get request DELETE the path /remove')
+})
 
-
-
+// GET all items
+app.get('/items', (req,res)=>{
+    res.json(data)
+})
+// GET specific item
+app.get('/item/:id', (req,res)=>{
+    res.json(data[req.params.id-1])
+})
 
 // establish the server
 app.listen(PORT,()=>{
     console.log(`Your server is running on port : ${PORT}`)
+    // console.log(data)
 })
